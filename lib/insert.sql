e224b3396f0f527c3665c9b45c98c250801f3e6b
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-11-06 06:53:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy"
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "Female", "Black", "Active", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 27, "Male", "Brown", "Passive", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "Female", "White", "Playful", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 27, "Male", "Brown", "Passive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 22, "Male", "Black", "Grumpy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 27, "Male", "Brown", "Passive", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 27, "Male", "Brown", "Passive", 0);
