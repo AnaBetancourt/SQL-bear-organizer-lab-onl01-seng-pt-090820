8d6eb0bf26ecb4f43278ca58706b048ce29075f8
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-11-06 07:06:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "Male", "Brown", "Easygoing", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "White", "Excitable", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "Female", "Black", "Active", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 27, "Male", "Brown", "Passive", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "Female", "White", "Playful", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 22, "Male", "Black", "Grumpy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 30, "Female", "Brown", "Social", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 16, "Male", "Brown", "Aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "Brown", "Easygoing", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "M", "White", "Excitable", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "F", "Black", "Active", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 27, "M", "Brown", "Passive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "F", "White", "Playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 22, "M", "Black", "Grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 30, "F", "Brown", "Social", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 16, "M", "Brown", "Aggressive", 0);
