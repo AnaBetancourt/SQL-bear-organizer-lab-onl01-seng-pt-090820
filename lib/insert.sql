c2886bdd4954d1ff8027b50c2a7995b3bc8c756e
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-11-06 06:45:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Brown", "Calm", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "Brown", "Excitable", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "Female", "Brown", "Excitable", 1);
